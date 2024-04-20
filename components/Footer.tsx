4376dbd929a653b2f3fd86411d006cdd22184e81
tky0202 <tky0202mail@gmail.com>
2024-04-20 20:41:15 +0900

    update

---
+++ components/Footer.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from 'react';
+import { BottomNavigationAction } from '@mui/material';
+import { Home, Person, Article, AddLocationAlt } from '@mui/icons-material';
+import Link from 'next/link';
+import styles from '@/app/page.module.css'
+import { usePathname } from 'next/navigation';
+
+const Footer = () => {
+  const pathname = usePathname();
+
+  return (
+    <footer>
+            <BottomNavigationAction
+              LinkComponent={Link}
+              href="/"
+              icon={<Home />}
+              disabled={pathname === '/'}
+              label="HOME"
+              className={`${styles.footerAction} ${pathname === '/' ? '' : styles.footerActionDisabled}`}
+            />
+            <BottomNavigationAction
+              LinkComponent={Link}
+              href="/map"
+              icon={<AddLocationAlt />}
+              disabled={pathname === '/map'}
+              label="電車スポット"
+              className={`${styles.footerAction} ${pathname === '/map' ? '' : styles.footerActionDisabled}`}
+            />
+            <BottomNavigationAction
+              LinkComponent={Link}
+              href="/static"
+              icon={<Article />}
+              disabled={pathname === '/static'}
+              label="電車情報"
+              className={`${styles.footerAction} ${pathname === '/static' ? '' : styles.footerActionDisabled}`}
+            />
+           <BottomNavigationAction
+              LinkComponent={Link}
+              href="/introduction"
+              icon={<Person />}
+              disabled={pathname === '/introduction'}
+              label="管理者"
+              className={`${styles.footerAction} ${pathname === '/introduction' ? '' : styles.footerActionDisabled}`}
+            />
+          <p className={`${styles.footerCopy}`}>
+            ©️ tky0202
+          </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from 'react';
+import { IconButton, Drawer, List, ListItem, ListItemText } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import styles from '@/app/page.module.css';
+import Link from 'next/link';
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: any) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(open);
+  };
+
+  return (
+    <>
+      <header>
+        <Link href="/" className={styles.titleH1Link}><h1 className={styles.titleH1}>子供と一緒に電車デビュー</h1></Link>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer(true)} className={styles.titleH1Nav}><MenuIcon /></IconButton>
+      </header>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+        <List>
+          <ListItem button key={'HOME'}>
+            <Link href="/">
+              <ListItemText primary={'HOME'} />
+            </Link>
+          </ListItem>
+          <ListItem button key={'電車スポット'}>
+            <Link href="/map">
+              <ListItemText primary={'電車スポット'} />
+            </Link>
+          </ListItem>
+          <ListItem button key={'管理者'}>
+            <Link href="/introduction">
+              <ListItemText primary={'管理者'} />
+            </Link>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
+  );
+};
+
+
+export default Header;
