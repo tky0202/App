ccf8e6eae0acf730bf04a1c7064ee178b6ff3765
tky0202 <tky0202mail@gmail.com>
2024-04-30 22:48:28 +0900

    update

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { YouTubeEmbed } from "@next/third-parties/google";
 
-
 const EmblaCarousel = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(0); // 初期値を設定
 
-  // ランダムなYouTube動画のIDを生成
-  const generateRandomVideoIds = (count: number) => {
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // クライアントサイドでのみリサイズイベントをリスニングする
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize);
+      }
+    };
+  }, []);
+
+  const videoCount = windowWidth >= 600 ? 3 : 1;
+
+  const generateRandomVideoIds = (count: number): string[] => {
     const videoIds = [
       "M2Zm9JT4cUs",
       "nl9WikoK8hU",
@@ -24,20 +43,28 @@ const EmblaCarousel = () => {
     return shuffledIds.slice(0, count);
   };
 
+  const getVideoStyle = (): string => {
+    if (windowWidth >= 600) {
+      return "width: 363px; height: 250px; margin: 0 1rem;";
+    } else {
+      return "width: 100vw; height: 210px; margin: 0rem;margin-top:-3%;";
+    }
+  };
+
   return (
     <div className="embla">
-        <div className="embla__container">
-          {generateRandomVideoIds(window.innerWidth >= 600 ? 3 : 1).map((videoId, index) => (
+      <div className="embla__container">
+        {generateRandomVideoIds(videoCount).map((videoId, index) => (
           <YouTubeEmbed
-          key={index}
-          videoid={videoId}
-          style={window.innerWidth >= 600 ? "width:363px;height:250px;margin:0 1rem;" : "width:100vw;height:210px;margin:0rem;margin-top: -3%;"}
-          params="controls=0&autoplay=1&mute=1&rel=0"
-        />
-          ))}
-        </div>
+            key={index}
+            videoid={videoId}
+            style={getVideoStyle()}
+            params="controls=0&autoplay=1&mute=1&rel=0"
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmblaCarousel
+export default EmblaCarousel;
