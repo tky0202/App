ea5dbac9afa9e587bb13090779d41c65a0a83833
tky0202 <tky0202mail@gmail.com>
2024-04-30 19:48:52 +0900

    update

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -1,95 +1,41 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { EmblaCarouselType, EmblaOptionsType } from 'embla-carousel'
-import useEmblaCarousel from 'embla-carousel-react'
-import Youtube from 'react-youtube';
-import {
-  NextButton,
-  PrevButton,
-  usePrevNextButtons
-} from './EmblaCarouselArrowButtons'
-import { DotButton, useDotButton } from './EmblaCarouselDotButton'
-
-type PropType = {
-  slides: number[]
-  options?: EmblaOptionsType
-}
+import React from 'react'
+import { YouTubeEmbed } from "@next/third-parties/google";
 
 
-const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options } = props
-  const [emblaRed, emblaApi] = useEmblaCarousel(options)
-  const [slidesInView, setSlidesInView] = useState<number[]>([])
+const EmblaCarousel = () => {
 
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
-
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
-
-  const updateSlidesInView = useCallback((emblaApi: EmblaCarouselType) => {
-    setSlidesInView((slidesInView) => {
-      if (slidesInView.length === emblaApi.slideNodes().length) {
-        emblaApi.off('slidesInView', updateSlidesInView)
-      }
-      const inView = emblaApi
-        .slidesInView()
-        .filter((index) => !slidesInView.includes(index))
-      return slidesInView.concat(inView)
-    })
-  }, [])
-
-  const opts = {
-    playerVars: {
-    playsinline: 1,
-    controls: 1,       
-    autoplay: 1,
-    mute: 1,
-    rel:0,
-    }, 
-  }
-
-  useEffect(() => {
-    if (!emblaApi) return
-
-    updateSlidesInView(emblaApi)
-    emblaApi.on('slidesInView', updateSlidesInView)
-    emblaApi.on('reInit', updateSlidesInView)
-  }, [emblaApi, updateSlidesInView])
+  // ランダムなYouTube動画のIDを生成
+  const generateRandomVideoIds = (count: number) => {
+    const videoIds = [
+      "M2Zm9JT4cUs",
+      "nl9WikoK8hU",
+      "Q1rShU0lM0g",
+      "65SET8xj6Ro",
+      "T3S0gqxXjWY",
+      "lNjzPQCjIkk",
+      "Vry3SCavQ6Q",
+      "0dfWjQBozm4",
+      "TU8q12FHpn0",
+      "rxR7ljbmBTs",
+      "g54AzBEeioM",
+      "E67brG2nSLs"
+    ];
+    const shuffledIds = videoIds.sort(() => Math.random() - 0.5);
+    return shuffledIds.slice(0, count);
+  };
 
   return (
     <div className="embla">
-      <div className="embla__viewport" ref={emblaRed}>
         <div className="embla__container">
-          <Youtube videoId="M2Zm9JT4cUs" opts={opts} className="embla_iframe" />
-          <Youtube videoId="nl9WikoK8hU" opts={opts} className="embla_iframe" />
-          <Youtube videoId="Q1rShU0lM0g" opts={opts} className="embla_iframe" />
-          <Youtube videoId="65SET8xj6Ro" opts={opts} className="embla_iframe" />
-          <Youtube videoId="T3S0gqxXjWY" opts={opts} className="embla_iframe" />
-        </div>
-      </div>
-
-      <div className="embla__controls">
-        <div className="embla__buttons">
-          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
-
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
-              )}
-            />
+          {generateRandomVideoIds(window.innerWidth >= 600 ? 3 : 1).map((videoId, index) => (
+          <YouTubeEmbed
+          key={index}
+          videoid={videoId}
+          style={window.innerWidth >= 600 ? "width:363px;height:250px;margin:0 1rem;" : "width:100vw;height:210px;margin:0rem;margin-top: -3%;"}
+          params="controls=0&autoplay=1&mute=1&rel=0"
+        />
           ))}
         </div>
-      </div>
     </div>
   )
 }
